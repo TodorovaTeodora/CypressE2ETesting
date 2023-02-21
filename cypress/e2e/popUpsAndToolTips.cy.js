13a42bc644a04e35ad77d5ca53d7427c3b127860
TodorovaTeodora <57221159+TodorovaTeodora@users.noreply.github.com>
2023-02-21 18:09:24 +0200

    Update popUpsAndToolTips.cy.js

--- cypress/e2e/popUpsAndToolTips.cy.js
+++ cypress/e2e/popUpsAndToolTips.cy.js
@@ -17,6 +17,7 @@ it('dialog box', () => {
     cy.contains('Tables & Data').click()
     cy.contains('Smart Table').click()
         
+    //we locate the bin element located inside the table body and click on it:
     cy.get('tbody tr').first().find('.nb-trash').click()
     //if the alert window doesn't show up the stub will be an empty object of window:confirm so when we try to make a get call we won't have a message
     const stub = cy.stub()
