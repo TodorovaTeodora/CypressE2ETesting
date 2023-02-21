742bc2b53cd8cb6142862289a0c3f5d88055dacc
TodorovaTeodora <57221159+TodorovaTeodora@users.noreply.github.com>
2023-02-21 18:14:03 +0200

    Update radioButtonsAndCheckboxTests.cy.js

--- cypress/e2e/radioButtonsAndCheckboxTests.cy.js
+++ cypress/e2e/radioButtonsAndCheckboxTests.cy.js
@@ -1,4 +1,4 @@
-//How to work with radio buttons
+//How to work with radio buttons:
 
 it('radio button', () => {
     it('assert property', ()=> {
@@ -9,7 +9,7 @@ it('radio button', () => {
         cy.contains('nb-card', 'Using the grid').find('[type="radiobutton"]').then( radioButttons => {
             cy.wrap(radioButttons)
             .first()
-            //using {force:true} we disable the cypress default check for the element to be visible
+  //using {force:true} we disable the cypress default check for the element to be visible
             .check({force: true})
             .should('be.checked')
 
@@ -27,14 +27,14 @@ it('radio button', () => {
     })
 })
 
-//How to work with checkboxes
+//How to work with checkboxes:
 
 it('check boxes', () => {
         cy.visit('/')
         cy.contains('Modal & Overlays').click()
         cy.contains('Toastr').click()
 
-    //the check method only checks but cannot uncheck boxes and radio buttons, to uncheck we use click()
+ //the check() method only checks but cannot uncheck boxes and radio buttons, to uncheck we use click()
         cy.get('[type="checkbox"]').check({force: true})
         cy.get('[type="checkbox"]').eq(0).click({force: true})
 
